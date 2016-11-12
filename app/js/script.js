4f2594aa335becf8e2bcc2f2f1ce4fa81a61572e
KlaraSporrong <klara.sporrong@gmail.com>
2016-11-12 15:55:21 +0100

    added side menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,29 @@
 angular.module('BlankApp', ['ngMaterial', 'ngMap'])
-.controller('AppCtrl', function(NgMap,$scope){
+.controller('AppCtrl', function(NgMap,$scope, $mdSidenav){
 	$scope.googleMapsUrl="https://maps.googleapis.com/maps/api/js?key=AIzaSyBq_v_JvxmZ_56MWWcGjL1KUlIb3_Ki5Nc";
 	NgMap.getMap().then(function(map) {
-    console.log(map.getCenter());
-    console.log('markers', map.markers);
-    console.log('shapes', map.shapes);
-    $scope.pos = {
+	    console.log(map.getCenter());
+	    console.log('markers', map.markers);
+	    console.log('shapes', map.shapes);
+  	});
+	$scope.pos = {
     	lat: 59.3341577,
     	lng: 18.0745626,
     	zoom: 16
     }
-  });
+    $scope.isOpen = false;
+    $scope.togglePlusMenu = function(){
+    	if($scope.isOpen) {
+    		$scope.isOpen = false;
+    	} else {
+    		$scope.isOpen = true;
+    	}
+    }
+    
+    $scope.toggleLeft = buildToggler('left');
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+      }
+    }
 });
