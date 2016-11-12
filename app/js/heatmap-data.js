b4b03d5bbd9f9dbe01d320ee9b48c65cb1ba2bf9
Maja Engvall <majaengvall@Majas-MacBook-Pro.local>
2016-11-12 15:44:33 +0100

    heatmap data

---
+++ app/js/heatmap-data.js
@@ -0,0 +1,12 @@
+var heatmapData = [
+  new google.maps.LatLng(59.3641577, 18.0945626),
+  new google.maps.LatLng(59.3681577, 18.0345626),
+  new google.maps.LatLng(59.3741577, 18.0565626),
+  new google.maps.LatLng(59.3241577, 18.0445626),
+  new google.maps.LatLng(59.3441577, 18.0345626),
+  new google.maps.LatLng(59.3841577, 18.0845626),
+  new google.maps.LatLng(59.3141577, 18.0145626),
+  new google.maps.LatLng(59.3571577, 18.0945626),
+  new google.maps.LatLng(59.3681577, 18.0335626),
+  new google.maps.LatLng(59.3621577, 18.0215626)
+];
